a396cbc43b6bf04a11190eb130797b39f60db071
Anjie Wang <anjiewg@gmail.com>
2020-07-18 19:12:25 +0000

    added all functions from arithmetic.py

--- calculator.py
+++ calculator.py
@@ -7,13 +7,32 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 # Replace this with your code
 
 while True:
-    input_string = input("Enter your equation :")
+    input_string = input("Enter your equation: ")
     tokenized = input_string.split(" ")
-
     if tokenized[0] == "q":
         print("Exiting")
         break
-    
+    else:
+        num1 = float(tokenized[1])
+        num2 = float(tokenized[2])
+        if tokenized[0] == "+":
+            print(add(num1, num2))
+        elif tokenized[0] == "-":
+            print(subtract(num1, num2))
+        elif tokenized[0] == "*":
+            print(multiply(num1, num2))
+        elif tokenized[0] == "/":
+            print(divide(num1, num2))
+        elif tokenized[0] == "square":
+            print(square(num1))
+        elif tokenized[0] == "cube":
+            print(cube(num1))
+        elif tokenized[0] == "power":
+            print(power(num1, num2))
+        elif tokenized[0] == "mod":
+            print(mod(num1, num2))
+
+
 
 
 
